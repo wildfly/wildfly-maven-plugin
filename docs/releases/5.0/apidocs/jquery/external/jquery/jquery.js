66303c43b5f14382b173218198bb0c281394745c
James R. Perkins <jperkins@redhat.com>
2023-12-19 17:02:34 -0800

    Add release documentation
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- docs/releases/5.0/apidocs/jquery/external/jquery/jquery.js
+++ docs/releases/5.0/apidocs/jquery/external/jquery/jquery.js
@@ -1,6 +1,15 @@
-/*
- * Copyright The WildFly Authors
- * SPDX-License-Identifier: Apache-2.0
+/*!
+ * jQuery JavaScript Library v3.5.1
+ * https://jquery.com/
+ *
+ * Includes Sizzle.js
+ * https://sizzlejs.com/
+ *
+ * Copyright JS Foundation and other contributors
+ * Released under the MIT license
+ * https://jquery.org/license
+ *
+ * Date: 2020-05-04T22:49Z
  */
 ( function( global, factory ) {
 
